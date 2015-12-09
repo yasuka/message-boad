20817617f0b24afb9ee095fa029fb219a558bcf4
yasuka <satoumirin7@gmail.com>
2015-12-09 08:41:08 +0000

    syusei

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
     #名前は必須入力かつ20文字以内
     validates :name , length: { maximum: 20 } , presence: true
     #年齢は必須入力かつ0以上200以内
-    validates :age , numericality: { greater_than: 0 , less_than: 200 } , presence: true
+    validates :age , numericality: { greater_than_or_equal_to: 0 , less_than: 200 } , presence: true
     #内容は必須入力かつ2文字以上30文字以下
     validates :body , length: { minimum: 2 , maximum:30 } , presence: true
 end
